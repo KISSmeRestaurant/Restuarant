399e9df1824651ac8ca39aab1f147068af093282
KISHORE-14622 <kishoresathosh14622.com>
2025-06-26 22:25:58 +0530

    Updated project files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://restuarant-sh57.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false, // Optional: if you need to bypass SSL verification
