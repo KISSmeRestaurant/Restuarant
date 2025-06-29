9237fc94990a44cd159f8d77a04422511dc41af5
KISHORE-14622 <kishoresanthosh14622@gmail.com>
2025-06-29 10:59:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  uild: {
+    outDir: '.dist' // Add this line
+  },
   server: {
     proxy: {
       '/api': {
