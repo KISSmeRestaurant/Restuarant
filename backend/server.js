d19ae800e9e63c9e5f6d16685f40c7910c07d541
KISHORE-14622 <kishoresanthosh14622@gmail.com>
2025-06-29 11:29:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ connectDB();
 // Security Middleware
 securityMiddleware(app);
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'||'https://restuarant-frontend-kjeu.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
